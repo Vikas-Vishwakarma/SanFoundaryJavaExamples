ec85af3d69d8bd1c6ad34ba79a2af95588dcca4e
Vikas Vishwakarma <vikaspvishwakarma@gmail.com>
2022-09-04 17:34:36 +0530

    everse a Number and Find Sum using do-while Loop

---
+++ Numbers/Sum_Reverse_Number.java
@@ -0,0 +1,26 @@
+//Java Program to Reverse a Number and Find its Sum using do-while Loop
+
+package sanfoundary.Numbers;
+
+import java.util.Scanner;
+
+public class Sum_Reverse_Number {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Java Program to Reverse a Number and Find its Sum using do-while Loop");
+		System.out.print("Enter a Number: ");
+		int n = sc.nextInt();
+		sc.close();
+		int sum =0;
+		
+		do {
+			int rem = n% 10;
+			sum += rem;
+			n = n/10;
+		}while(n > 0);
+		
+		System.out.println(sum);
+		
+	}
+
+}
